ab7342b4c40ef84ace1cb8fe104d08343f12f391
Santosh Bashyal <77581468+santoshbashyal@users.noreply.github.com>
2023-12-20 17:41:09 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// for (let i =0; i<5; i++)
+// {
+//     console.log(i)
+// }
+
+
+// **************************************************************************8
+
+// PROGRAM TO ADD FIRST N NATURAL NUMBERS
+
+// let sum = 0;
+
+// for(let i = 0; i<=4; i++){
+//     sum = sum+i;
+// }
+// console.log(sum)
+// // ***********************************************************************
+
+
+
+// // FOR-IN loop
+
+// let obj = {
+//     harry : 90,
+//     santosh:99,
+//     ritika :99,
+//     shivika: 91,
+// }
+
+// for(let key in obj){
+//     console.log(key + ':' , obj[key]);
+
+// }
+
+// ******************************************************************
+
+//  WHILE LOOP
+
+let n = 10;
+let i = 0;
+
+while(i<n){
+    console.log(i)
+    i++;
+}
+
+
+
+
+
